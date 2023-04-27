d3cc3bcfb55bc19769070421745463e55c466ffd
Egor <Kapitonov281087@mail.ru>
2023-04-27 17:29:09 +0900

    HW-1 add def import_in_customers_data

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,13 +1,26 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import csv
 import psycopg2
+import os
+
+password_bd: str = os.getenv('password_bd')
 
 
-def import_in_customers_data():
+def import_in_customers_data(url_file='./north_data/customers_data.csv'):
     """
     Импортирует из customers_data.csv в БД одноименную таблицу
     """
-    pass
+    with open(url_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        with psycopg2.connect(host="localhost", database="north", user="postgres", password=password_bd) as conn:
+            with conn.cursor() as cur:
+                print(cur)
+                for i in reader:
+                    print(cur)
+                    print(i["customer_id"], i["company_name"], i["contact_name"])
+                    cur.execute("INSERT INTO customers_data VALUES (%s, %s, %s)",
+                                (i["customer_id"], i["company_name"], i["contact_name"]));
+        conn.close()
 
 
 def import_in_employees_data():
@@ -22,3 +35,6 @@ def import_in_orders_data():
     Импортирует из orders_data.csv в БД одноименную таблицу
     """
     pass
+
+
+import_in_customers_data()
